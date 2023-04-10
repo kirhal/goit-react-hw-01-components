80bbe1dba898d87a9b61d63cd6e5321ec9926cf6
kirhal <kirillgaleckiy@gmail.com>
2023-04-10 20:50:27 +0200

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -29,12 +29,9 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
-
       <FriendList friends={friends} />
-
       <TransactionHistory items={transactions} />
     </div>
   );
